981760179be2da6dc5585461bd7ed15135fe3b9c
Alan Alexis Moreno Martinez <alanalexismartinez@gmail.com>
2021-02-01 10:47:34 -0600

    firs commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+//Mis importaciones
+import 'package:zombifi_app/src/pages/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Zombifi App',
+      debugShowCheckedModeBanner: false,
+      home: HomePage()
+    );
+  }
+}
