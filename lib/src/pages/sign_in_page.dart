bb6880ef40ed4b06bb4a55f1aa28b5cf33ab2b17
Alan Alexis Moreno Martinez <alanalexismartinez@gmail.com>
2021-02-02 12:16:24 -0600

    Creación de los métodos que van retornar los widgets que componen la page

--- lib/src/pages/sign_in_page.dart
+++ lib/src/pages/sign_in_page.dart
@@ -1,90 +1,82 @@
 import 'package:flutter/material.dart';
 
+//Importaciones propias
+import 'package:zombifi_app/widgets/button_round.dart';
+import 'package:zombifi_app/widgets/text_form_field.dart';
+
 class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Form(
         child: Column(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(top: 25),
-              child: TextFormField(
-                autocorrect: true,
-                keyboardType: TextInputType.emailAddress,
-                controller: null,
-                style: TextStyle(color: Colors.black),
-                decoration: InputDecoration(
-                  labelText: 'Correo electrónico',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0)),
-                  hintText: 'zombifi@app.com',
-                  // border: ,
-                  // icon: Icon(Icons.person_outline_rounded)
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: TextFormField(
-                obscureText: true,
-                controller: null,
-                style: TextStyle(color: Colors.black),
-                decoration: InputDecoration(
-                  labelText: 'Contraseña',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0)),
-                  hintText: '*****',
-                  // border: ,
-                  // icon: Icon(Icons.lock_outline_rounded)
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 40),
-              child: MaterialButton(
-                onPressed: () {},
-                child: Text(
-                  'Olvide mi contraseña',
-                  style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black),
-                ),
-                color: Colors.transparent,
-                elevation: 0,
-                minWidth: 400,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: MaterialButton(
-                onPressed: () {},
-                child: Text(
-                  'CONTINUAR',
-                  style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                ),
-                color: Colors.greenAccent,
-                elevation: 0,
-                minWidth: 400,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-              ),
-            ),
-          ],
+          children: <Widget>[_widgetsLogin()],
         ),
       ),
-      // child: Center(
-      //   child: Text('Inicio de sesión'),
-      // ),
     );
   }
+
+  _textFormFields() => Container(
+        child: Column(
+          children: <Widget>[
+            Padding(padding: EdgeInsets.symmetric(vertical: 20)),
+            _email(),
+            Padding(padding: EdgeInsets.only(top: 18)),
+            _password(),
+          ],
+        ),
+      );
+  //TextFormField para el email
+  TextFormFieldWidget _email() {
+    return TextFormFieldWidget(
+      controller: null,
+      keyboardType: TextInputType.emailAddress,
+      icon: Icons.person_outline_rounded,
+      hintText: 'ejemplo@correo.com',
+      lableText: 'Correo',
+      obscureText: false,
+    );
+  }
+
+  //TextFormField para el password
+  TextFormFieldWidget _password() {
+    return TextFormFieldWidget(
+      controller: null,
+      obscureText: true,
+      icon: Icons.lock_outline_rounded,
+      lableText: 'Contraseña',
+    );
+  }
+
+  //Método que será compuesto por los métodos que serán los que van a componer
+  //el body de la vista
+  _widgetsLogin() => Container(
+        child: Form(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.spaceAround,
+            // mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              _textFormFields(),
+              Padding(padding: EdgeInsets.only(top: 25)),
+              _buttons(),
+            ],
+          ),
+        ),
+      );
+
+  //Método que importa el widget personalizado ButtonRound,
+  //y se le pasan los parametros que lo van a componer
+  //ButtonRoundWidget(descripcion, fontSize, fontWeight, colorText,
+  //  colorButton, elevetion, height, minWidth)
+  _buttons() => Container(
+        child: Column(
+          children: <Widget>[
+            ButtonRoundWidget('Olvidé mi contraseña', 15, FontWeight.bold,
+                Colors.black, Colors.transparent, 0, 60, 350),
+            Padding(padding: EdgeInsets.only(top: 13)),
+            ButtonRoundWidget('INICIAR SESIÓN', 15, FontWeight.bold,
+                Colors.black, Colors.greenAccent, 0, 60, 350),
+          ],
+        ),
+      );
 }
