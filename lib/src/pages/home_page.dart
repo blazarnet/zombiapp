1a3b207ed0e1b9a2dd01674f0152eed280a692c6
Alan Alexis Moreno Martinez <alanalexismartinez@gmail.com>
2021-02-03 15:43:55 -0600

    Modificación del home page, para visualizar el estado de fichas y el usuario loggeado, todo como un demo

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,12 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:zombifi_app/widgets/card_status_on.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
-        title: Text('¡Bienvenido!'),
+        automaticallyImplyLeading: false,
+        leading: _avatar(),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: Icon(
+              Icons.notifications,
+              color: Color.fromRGBO(112, 112, 112, 1),
+            ),
+          )
+        ],
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: Container(
+        width: size.width * 1,
+        color: Colors.white,
+        child: Column(
+          // crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(padding: EdgeInsets.all(7)),
+            _welcome(),
+            Container(
+              width: size.width * .93,
+              color: Colors.white,
+              child: Column(
+                children: <Widget>[_statusOnWidget()],
+              ),
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  Widget _avatar() {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 10,
+      ),
+      child: CircleAvatar(
+        child: Text('SL'),
+      ),
+    );
+  }
+
+  Widget _welcome() {
+    return Row(
+      children: <Widget>[
+        Stack(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(left: 12),
+                  child: Text(
+                    'Hola,',
+                    style: TextStyle(fontSize: 25),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 12),
+                  child: Text(
+                    'Alan Moreno Martinez',
+                    style: TextStyle(fontSize: 25),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  CardStatusOnWidget _statusOnWidget() {
+    return CardStatusOnWidget();
   }
 }
