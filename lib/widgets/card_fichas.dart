2ad287ca6c166d492e7bc4b55e369cd6ac480eff
Alan Alexis Moreno Martinez <alanalexismartinez@gmail.com>
2021-02-08 13:54:27 -0600

    Creación del widget para las fichas en venta

---
+++ lib/widgets/card_fichas.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class CardFichasWidget extends StatefulWidget {
+  final Widget child;
+
+  const CardFichasWidget({this.child});
+
+  @override
+  _CardFichasWidgetState createState() => _CardFichasWidgetState();
+}
+
+class _CardFichasWidgetState extends State<CardFichasWidget> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        elevation: 2,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        color: Colors.white,
+        child: widget.child);
+  }
+}
