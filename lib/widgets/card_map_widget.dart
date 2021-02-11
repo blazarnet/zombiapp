12335dfcb777e15e57d068613960a74aaeeada0d
Alan Alexis Moreno Martinez <alanalexismartinez@gmail.com>
2021-02-11 15:49:43 -0600

    mend

--- lib/widgets/card_map_widget.dart
+++ lib/widgets/card_map_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:zombifi_app/generated/l10n.dart';
+import 'package:zombifi_app/src/pages/fullscreen_map.dart';
 
 class CardMapWidget extends StatefulWidget {
   @override
@@ -15,20 +16,29 @@ class _MapWidgetState extends State<CardMapWidget> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Card(
-      elevation: 15,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
-      child: Container(
-        alignment: Alignment.bottomLeft,
-        width: size.width * .90,
-        height: size.height * .15,
-        decoration: BoxDecoration(
-            color: Colors.grey, borderRadius: BorderRadius.circular(20)),
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Text(
-            AppLocalizations.of(context).exploreZones,
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+    return FlatButton(
+      onPressed: () {
+        final route = MaterialPageRoute(builder: (context) {
+          return FullScreenMap();
+        });
+        Navigator.push(context, route);
+      },
+      child: Card(
+        elevation: 15,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+        child: Container(
+          alignment: Alignment.bottomLeft,
+          width: size.width * .90,
+          height: size.height * .15,
+          decoration: BoxDecoration(
+              color: Colors.grey, borderRadius: BorderRadius.circular(20)),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Text(
+              AppLocalizations.of(context).exploreZones,
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
           ),
         ),
       ),
