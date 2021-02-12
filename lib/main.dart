f994a2acfcfe7e9395f518a243d041658e1003ae
Alan Alexis Moreno Martinez <alanalexismartinez@gmail.com>
2021-02-12 15:34:05 -0600

    Cambio de pagina de inicio, de animation page a splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 //Mis importaciones
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:zombifi_app/generated/l10n.dart';
-import 'package:zombifi_app/src/pages/animation_page.dart';
+import 'package:zombifi_app/src/pages/splash_screen.dart';
 // import 'package:flutter_svg/flutter_svg.dart';
 // import 'package:zombifi_app/src/pages/login_page.dart';
 
@@ -13,15 +13,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Zombifi App',
-        debugShowCheckedModeBanner: false,
-        localizationsDelegates: [
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-          AppLocalizations.delegate
-        ],
-        supportedLocales: AppLocalizations.delegate.supportedLocales,
-        home: AnimationPage());
+      title: 'Zombifi App',
+      debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        AppLocalizations.delegate
+      ],
+      supportedLocales: AppLocalizations.delegate.supportedLocales,
+      home: SplashScreen(),
+    );
   }
 }
