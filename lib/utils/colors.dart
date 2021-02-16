07b0a67b816e4bf2519bbcddbda36a89861136dd
Alan Alexis Moreno Martinez <alanalexismartinez@gmail.com>
2021-02-16 09:46:44 -0600

    Adición del archivo colors para tener un control sobre los colores que estarán en toda la app y también el archivo utils para hacer las exportación de los archivos en la carpeta utils

---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+List<Color> colorGradient = <Color>[
+  Color.fromRGBO(7, 227, 131, .80),
+  Color.fromRGBO(0, 126, 70, .70),
+];
+
+Color colorTextSign = Color.fromRGBO(8, 104, 37, 1);
