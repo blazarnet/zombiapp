c42099219c97258c36036c5d996e9f842f7f9f75
Alan Alexis Moreno Martinez <alanalexismartinez@gmail.com>
2021-02-17 11:29:07 -0600

    Se cambiaron los valores de algunos colores, usando los colores oficiales

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'dart:ui';
 
 List<Color> colorGradient = <Color>[
-  Color.fromRGBO(7, 227, 131, .80),
-  Color.fromRGBO(0, 126, 70, .70),
+  Color.fromRGBO(7, 236, 136, 1),
+  Color.fromRGBO(0, 125, 70, 1),
 ];
 
-Color colorTextSign = Color.fromRGBO(8, 104, 37, 1);
 Color colorGreen = Color.fromRGBO(0, 185, 99, 1);
+Color colorBarrier = Color.fromRGBO(117, 117, 117, .5);
