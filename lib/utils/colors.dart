ece259bcdab6e0400b9a369751a2ffc21c52e6c9
Alan Alexis Moreno Martinez <alanalexismartinez@gmail.com>
2021-02-18 14:57:37 -0600

    Adición de variables de color para usar en toda la app

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 import 'dart:ui';
 
-List<Color> colorGradient = <Color>[
+List<Color> colorGradientGreen = <Color>[
   Color.fromRGBO(7, 236, 136, 1),
   Color.fromRGBO(0, 125, 70, 1),
 ];
 
+List<Color> colorGradientYellow = <Color>[
+  Color.fromRGBO(253, 229, 9, 1),
+  Color.fromRGBO(192, 173, 0, 1),
+];
+
+List<Color> colorGradientGrey = <Color>[
+  Color.fromRGBO(186, 186, 186, 1),
+  Color.fromRGBO(89, 89, 89, 1),
+];
+
 Color colorGreen = Color.fromRGBO(0, 185, 99, 1);
+Color colorMustard = Color.fromRGBO(190, 171, 0, 1);
 Color colorBarrier = Color.fromRGBO(117, 117, 117, .5);
