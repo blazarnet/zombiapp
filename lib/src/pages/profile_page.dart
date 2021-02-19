3ddf9b70b71ae9e0b563a1765e017611dceb42f6
Alan Alexis Moreno Martinez <alanalexismartinez@gmail.com>
2021-02-19 16:33:25 -0600

    Adición de los datos del usuario que podrán ser editables

--- lib/src/pages/profile_page.dart
+++ lib/src/pages/profile_page.dart
@@ -18,7 +18,7 @@ class _ProfilePageState extends State<ProfilePage> {
             pinned: true,
             flexibleSpace: FlexibleSpaceBar(
               // centerTitle: true,
-              collapseMode: CollapseMode.pin,
+              collapseMode: CollapseMode.parallax,
               titlePadding: EdgeInsets.only(left: 45),
               // title: Text(
               //   "Alan Moreno Martinez",
@@ -38,11 +38,12 @@ class _ProfilePageState extends State<ProfilePage> {
       body: Center(
         child: Container(
           child: Padding(
-            padding: EdgeInsets.fromLTRB(20, 0, 20, 500),
+            padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
             child: Container(
               child: SingleChildScrollView(
                 child: Column(
                   children: <Widget>[
+                    Padding(padding: EdgeInsets.only(top: 20)),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: <Widget>[
@@ -65,7 +66,9 @@ class _ProfilePageState extends State<ProfilePage> {
                         ),
                         _edit(),
                       ],
-                    )
+                    ),
+                    // Padding(padding: EdgeInsets.only(bottom: 50)),
+                    _data(context),
                   ],
                 ),
               ),
@@ -99,6 +102,142 @@ class _ProfilePageState extends State<ProfilePage> {
       minWidth: 100,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(30),
+      ),
+    );
+  }
+
+  Widget _data(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      height: size.height * 0.7,
+      padding: EdgeInsets.all(8.0),
+      child: Column(
+        children: <Widget>[
+          _cel(),
+          _email(),
+          _password(),
+          _ubication(),
+        ],
+      ),
+    );
+  }
+
+  ButtonRoundWidget _cel() {
+    return ButtonRoundWidget(
+      onPressed: () {},
+      elevetion: 0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30),
+      ),
+      height: 55,
+      colorButton: Color.fromRGBO(235, 235, 235, 1),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.phone_iphone_outlined,
+                  ),
+                  Text(AppLocalizations.of(context).phoneNumber)
+                ],
+              )
+            ],
+          ),
+          Icon(Icons.arrow_forward_ios)
+        ],
+      ),
+    );
+  }
+
+  ButtonRoundWidget _email() {
+    return ButtonRoundWidget(
+      onPressed: () {},
+      elevetion: 0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30),
+      ),
+      height: 55,
+      colorButton: Color.fromRGBO(235, 235, 235, 1),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                  ),
+                  Text(AppLocalizations.of(context).email)
+                ],
+              )
+            ],
+          ),
+          Icon(Icons.arrow_forward_ios)
+        ],
+      ),
+    );
+  }
+
+  ButtonRoundWidget _password() {
+    return ButtonRoundWidget(
+      onPressed: () {},
+      elevetion: 0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30),
+      ),
+      height: 55,
+      colorButton: Color.fromRGBO(235, 235, 235, 1),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.lock,
+                  ),
+                  Text(AppLocalizations.of(context).password)
+                ],
+              )
+            ],
+          ),
+          Icon(Icons.arrow_forward_ios)
+        ],
+      ),
+    );
+  }
+
+  ButtonRoundWidget _ubication() {
+    return ButtonRoundWidget(
+      onPressed: () {},
+      elevetion: 0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30),
+      ),
+      height: 55,
+      colorButton: Color.fromRGBO(235, 235, 235, 1),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  // Icon(
+                  //     // Icons,
+                  //     ),
+                  Text(AppLocalizations.of(context).ubication)
+                ],
+              )
+            ],
+          ),
+          Icon(Icons.arrow_forward_ios)
+        ],
       ),
     );
   }
