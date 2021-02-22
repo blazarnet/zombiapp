6c12adc784d4457de78173a1b4fba2245476cf8d
Alan Alexis Moreno Martinez <alanalexismartinez@gmail.com>
2021-02-22 11:04:29 -0600

    Rediseño en la vista del perfil para tener un circleAvatar y abrir un modal para seleccionar la foto de la galeria o de la camara

--- lib/src/pages/profile_page.dart
+++ lib/src/pages/profile_page.dart
@@ -1,6 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:zombifi_app/generated/l10n.dart';
+import 'package:zombifi_app/src/pages/home_page.dart';
+import 'package:zombifi_app/utils/colors.dart';
 import 'package:zombifi_app/widgets/button_round.dart';
+import 'package:zombifi_app/widgets/profile_photo.dart';
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -8,70 +14,86 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  nested() {
-    return NestedScrollView(
-      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-        return <Widget>[
-          SliverAppBar(
-            expandedHeight: 300.0,
-            floating: false,
-            pinned: true,
-            flexibleSpace: FlexibleSpaceBar(
-              // centerTitle: true,
-              collapseMode: CollapseMode.parallax,
-              titlePadding: EdgeInsets.only(left: 45),
-              // title: Text(
-              //   "Alan Moreno Martinez",
-              //   style: TextStyle(
-              //     color: Colors.white,
-              //     fontSize: 18,
-              //   ),
-              // ),
-              background: Image.asset(
-                "assets/man.png",
-                fit: BoxFit.contain,
-              ),
-            ),
-          )
-        ];
-      },
-      body: Center(
-        child: Container(
-          child: Padding(
-            padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-            child: Container(
-              child: SingleChildScrollView(
-                child: Column(
+  File photo;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      // body: nested(),
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            final route = MaterialPageRoute(
+              builder: (context) {
+                return HomePage();
+              },
+            );
+            Navigator.pop(context, route);
+          },
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: Container(
+        width: size.width * 1,
+        color: Colors.white,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: 5,
+            horizontal: 20,
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    Padding(padding: EdgeInsets.only(top: 20)),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              "Alan Moreno Martinez",
-                              style: TextStyle(
-                                fontSize: 20,
-                              ),
-                            ),
-                            Text(
-                              "Izamal-Yucatán",
-                              style: TextStyle(
-                                fontSize: 18,
-                              ),
-                            )
-                          ],
+                        Padding(
+                          padding: EdgeInsets.only(top: 7),
+                          child: Text(
+                            "Alan",
+                            style: TextStyle(fontSize: 22),
+                          ),
                         ),
-                        _edit(),
+                        Padding(
+                          padding: EdgeInsets.only(top: 7),
+                          child: Text(
+                            "Moreno Martinez",
+                            style: TextStyle(fontSize: 22),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 7.0),
+                          child: Text(
+                            "Izamal-Yucatán",
+                            style: TextStyle(
+                              fontSize: 18,
+                            ),
+                          ),
+                        )
                       ],
                     ),
-                    // Padding(padding: EdgeInsets.only(bottom: 50)),
-                    _data(context),
+                    GestureDetector(
+                      onTap: _onPressedPhoto,
+                      child: _photoWidget(),
+                    )
                   ],
                 ),
-              ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5),
+                  child: _data(context),
+                ),
+              ],
             ),
           ),
         ),
@@ -79,10 +101,9 @@ class _ProfilePageState extends State<ProfilePage> {
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: nested(),
+  ProfilePhotoWidget _photoWidget() {
+    return ProfilePhotoWidget(
+      photo: photo,
     );
   }
 
@@ -110,12 +131,15 @@ class _ProfilePageState extends State<ProfilePage> {
     final size = MediaQuery.of(context).size;
     return Container(
       height: size.height * 0.7,
-      padding: EdgeInsets.all(8.0),
       child: Column(
         children: <Widget>[
+          Padding(padding: EdgeInsets.only(top: 15)),
           _cel(),
+          Padding(padding: EdgeInsets.only(top: 15)),
           _email(),
+          Padding(padding: EdgeInsets.only(top: 15)),
           _password(),
+          Padding(padding: EdgeInsets.only(top: 15)),
           _ubication(),
         ],
       ),
@@ -240,5 +264,71 @@ class _ProfilePageState extends State<ProfilePage> {
         ],
       ),
     );
+  }
+
+  void _onPressedPhoto() {
+    showModalBottomSheet(
+      context: context,
+      elevation: 0,
+      barrierColor: colorBarrier,
+      builder: (context) {
+        return Container(
+          color: colorBarrier,
+          height: 170,
+          child: Container(
+            child: _buttons(),
+            decoration: BoxDecoration(
+              color: Theme.of(context).canvasColor,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buttons() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            trailing: Icon(Icons.add_a_photo),
+            title: Text(AppLocalizations.of(context).takePhoto),
+            subtitle: Text(AppLocalizations.of(context).openCamera),
+            onTap: _takePhoto,
+          ),
+          Divider(),
+          ListTile(
+            trailing: Icon(Icons.add_photo_alternate),
+            title: Text(AppLocalizations.of(context).selectPhoto),
+            subtitle: Text(AppLocalizations.of(context).openGallery),
+            onTap: _selectPhoto,
+          ),
+        ],
+      ),
+    );
+  }
+
+  _selectPhoto() async {
+    _processImage(ImageSource.gallery);
+  }
+
+  _takePhoto() async {
+    _processImage(ImageSource.camera);
+  }
+
+  _processImage(ImageSource origen) async {
+    // ignore: deprecated_member_use
+    photo = await ImagePicker.pickImage(source: origen);
+
+    if (photo != null) {
+      //Limpieza
+
+    }
+    setState(() {});
   }
 }
