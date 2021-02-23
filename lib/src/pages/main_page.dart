ac9f08bc5ea2803ae2bfc9d7c741ebf289c775fb
Alan Alexis Moreno Martinez <alanalexismartinez@gmail.com>
2021-02-23 13:48:39 -0600

    Se cambió el curved navigation bar por un bottom navigation bar, y se adicionó el item para el map

--- lib/src/pages/main_page.dart
+++ lib/src/pages/main_page.dart
@@ -1,10 +1,10 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:zombifi_app/src/pages/buy_chip_page.dart';
+import 'package:zombifi_app/generated/l10n.dart';
 import 'package:zombifi_app/src/pages/buy_credit_page.dart';
 import 'package:zombifi_app/src/pages/chips_page.dart';
 import 'package:zombifi_app/src/pages/home_page.dart';
 import 'package:zombifi_app/src/pages/profile_page.dart';
+import 'package:zombifi_app/src/pages/test_map_page.dart';
 import 'package:zombifi_app/utils/colors.dart';
 
 class MainPage extends StatefulWidget {
@@ -14,27 +14,90 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   int _currentIndex = 0;
+  // Color colorIcon = Colors.black;
   @override
   Widget build(BuildContext context) {
     double _size = 25;
     return Scaffold(
       body: _callPage(_currentIndex),
-      bottomNavigationBar: CurvedNavigationBar(
-        height: 50,
-        backgroundColor: Colors.white,
-        color: colorGreen,
-        index: _currentIndex,
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        items: <Widget>[
-          Icon(Icons.home, size: _size),
-          Icon(Icons.money_outlined, size: _size),
-          Icon(Icons.card_membership_outlined, size: _size),
-          Icon(Icons.person, size: _size),
-        ],
+      bottomNavigationBar: Container(
+        height: 60,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(35),
+            topRight: Radius.circular(35),
+          ),
+        ),
+        child: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          // index: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+              // colorIcon = colorIcon == colorIcon ? colorGreen : colorIcon;
+            });
+          },
+          selectedItemColor: colorGreen,
+          unselectedItemColor: Colors.black,
+          unselectedIconTheme: IconThemeData(color: Colors.black),
+          selectedIconTheme: IconThemeData(color: colorGreen),
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _currentIndex,
+          items: [
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                "assets/house-damage-solid.png",
+                height: 22,
+                width: 22,
+                // color: colorIcon,
+              ),
+              // ignore: deprecated_member_use
+              title: Text(AppLocalizations.of(context).home),
+            ),
+            BottomNavigationBarItem(
+              // icon: Icon(Icons.money_outlined, size: _size),
+              icon: Image.asset(
+                "assets/map-marked-alt-solid.png",
+                height: 22,
+                width: 22,
+                // color: colorIcon,
+              ),
+              // ignore: deprecated_member_use
+              title: Text(AppLocalizations.of(context).map),
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                "assets/ticket-alt-solid(1).png",
+                height: 22,
+                width: 22,
+                // color: colorIcon,
+              ),
+              // ignore: deprecated_member_use
+              title: Text(AppLocalizations.of(context).chips),
+            ),
+            BottomNavigationBarItem(
+              // icon: Icon(Icons.money_outlined, size: _size),
+              icon: Image.asset(
+                "assets/coins-solid.png",
+                height: 22,
+                width: 22,
+                // color: colorIcon,
+              ),
+              // ignore: deprecated_member_use
+              title: Text(AppLocalizations.of(context).credit),
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                "assets/user-circle-solid.png",
+                height: 22,
+                width: 22,
+                // color: colorIcon,
+              ),
+              // ignore: deprecated_member_use
+              title: Text(AppLocalizations.of(context).profile),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -44,10 +107,12 @@ class _MainPageState extends State<MainPage> {
       case 0:
         return HomePage();
       case 1:
-        return BuyCreditPage();
+        return TestMapPage();
       case 2:
         return ChipsPage();
       case 3:
+        return BuyCreditPage();
+      case 4:
         return ProfilePage();
 
       default:
