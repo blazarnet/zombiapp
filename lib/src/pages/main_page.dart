02926b845bae7fd70478f1e0ba44ea8b953f0b0a
Alan Alexis Moreno Martinez <alanalexismartinez@gmail.com>
2021-02-24 15:27:56 -0600

    Cambio del bottomNavigationBar para que tenga un diseño con los bordes superiores redondeados

--- lib/src/pages/main_page.dart
+++ lib/src/pages/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:zombifi_app/generated/l10n.dart';
 import 'package:zombifi_app/src/pages/buy_credit_page.dart';
 import 'package:zombifi_app/src/pages/chips_page.dart';
@@ -17,86 +18,151 @@ class _MainPageState extends State<MainPage> {
   // Color colorIcon = Colors.black;
   @override
   Widget build(BuildContext context) {
-    double _size = 25;
+    // final size = MediaQuery.of(context).size;
     return Scaffold(
       body: _callPage(_currentIndex),
       bottomNavigationBar: Container(
-        height: 60,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(35),
-            topRight: Radius.circular(35),
+        height: 80,
+        color: Colors.white,
+        child: Container(
+          height: 70,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30),
+              topRight: Radius.circular(30),
+            ),
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 10,
+                color: Colors.grey[500],
+                spreadRadius: .5,
+              )
+            ],
           ),
-        ),
-        child: BottomNavigationBar(
-          backgroundColor: Colors.white,
-          // index: _currentIndex,
-          onTap: (index) {
-            setState(() {
-              _currentIndex = index;
-              // colorIcon = colorIcon == colorIcon ? colorGreen : colorIcon;
-            });
-          },
-          selectedItemColor: colorGreen,
-          unselectedItemColor: Colors.black,
-          unselectedIconTheme: IconThemeData(color: Colors.black),
-          selectedIconTheme: IconThemeData(color: colorGreen),
-          type: BottomNavigationBarType.fixed,
-          currentIndex: _currentIndex,
-          items: [
-            BottomNavigationBarItem(
-              icon: Image.asset(
-                "assets/house-damage-solid.png",
-                height: 22,
-                width: 22,
-                // color: colorIcon,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              child: BottomNavigationBar(
+                elevation: 0,
+                backgroundColor: Colors.white,
+                // index: _currentIndex,
+                onTap: (index) {
+                  setState(() {
+                    _currentIndex = index;
+                  });
+                },
+                selectedItemColor: colorGreen,
+                unselectedItemColor: Colors.black,
+                unselectedIconTheme: IconThemeData(color: Colors.black),
+                selectedIconTheme: IconThemeData(color: colorGreen),
+                type: BottomNavigationBarType.fixed,
+                currentIndex: _currentIndex,
+                items: [
+                  BottomNavigationBarItem(
+                    activeIcon: Image.asset(
+                      "assets/house-damage-solid.png",
+                      height: 22,
+                      width: 22,
+                      color: colorGreen,
+                    ),
+                    icon: Image.asset(
+                      "assets/house-damage-solid.png",
+                      height: 22,
+                      width: 22,
+                    ),
+                    // ignore: deprecated_member_use
+                    title: Text(
+                      AppLocalizations.of(context).home,
+                      style: TextStyle(
+                        fontFamily: "gibson",
+                      ),
+                    ),
+                  ),
+                  BottomNavigationBarItem(
+                    activeIcon: Image.asset(
+                      "assets/map-marked-alt-solid.png",
+                      height: 22,
+                      width: 22,
+                      color: colorGreen,
+                    ),
+                    icon: Image.asset(
+                      "assets/map-marked-alt-solid.png",
+                      height: 22,
+                      width: 22,
+                    ),
+                    // ignore: deprecated_member_use
+                    title: Text(
+                      AppLocalizations.of(context).map,
+                      style: TextStyle(
+                        fontFamily: "gibson",
+                      ),
+                    ),
+                  ),
+                  BottomNavigationBarItem(
+                    activeIcon: Image.asset(
+                      "assets/ticket-alt-solid.png",
+                      height: 22,
+                      width: 22,
+                      color: colorGreen,
+                    ),
+                    icon: Image.asset(
+                      "assets/ticket-alt-solid.png",
+                      height: 22,
+                      width: 22,
+                    ),
+                    // ignore: deprecated_member_use
+                    title: Text(
+                      AppLocalizations.of(context).chips,
+                      style: TextStyle(
+                        fontFamily: "gibson",
+                      ),
+                    ),
+                  ),
+                  BottomNavigationBarItem(
+                    activeIcon: Image.asset(
+                      "assets/coins-solid.png",
+                      height: 22,
+                      width: 22,
+                      color: colorGreen,
+                    ),
+                    icon: Image.asset(
+                      "assets/coins-solid.png",
+                      height: 22,
+                      width: 22,
+                    ),
+                    // ignore: deprecated_member_use
+                    title: Text(
+                      AppLocalizations.of(context).credit,
+                      style: TextStyle(
+                        fontFamily: "gibson",
+                      ),
+                    ),
+                  ),
+                  BottomNavigationBarItem(
+                    activeIcon: Image.asset(
+                      "assets/user-circle-solid.png",
+                      height: 22,
+                      width: 22,
+                      color: colorGreen,
+                    ),
+                    icon: Image.asset(
+                      "assets/user-circle-solid.png",
+                      height: 22,
+                      width: 22,
+                    ),
+                    // ignore: deprecated_member_use
+                    title: Text(
+                      AppLocalizations.of(context).profile,
+                      style: TextStyle(
+                        fontFamily: "gibson",
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              // ignore: deprecated_member_use
-              title: Text(AppLocalizations.of(context).home),
             ),
-            BottomNavigationBarItem(
-              // icon: Icon(Icons.money_outlined, size: _size),
-              icon: Image.asset(
-                "assets/map-marked-alt-solid.png",
-                height: 22,
-                width: 22,
-                // color: colorIcon,
-              ),
-              // ignore: deprecated_member_use
-              title: Text(AppLocalizations.of(context).map),
-            ),
-            BottomNavigationBarItem(
-              icon: Image.asset(
-                "assets/ticket-alt-solid(1).png",
-                height: 22,
-                width: 22,
-                // color: colorIcon,
-              ),
-              // ignore: deprecated_member_use
-              title: Text(AppLocalizations.of(context).chips),
-            ),
-            BottomNavigationBarItem(
-              // icon: Icon(Icons.money_outlined, size: _size),
-              icon: Image.asset(
-                "assets/coins-solid.png",
-                height: 22,
-                width: 22,
-                // color: colorIcon,
-              ),
-              // ignore: deprecated_member_use
-              title: Text(AppLocalizations.of(context).credit),
-            ),
-            BottomNavigationBarItem(
-              icon: Image.asset(
-                "assets/user-circle-solid.png",
-                height: 22,
-                width: 22,
-                // color: colorIcon,
-              ),
-              // ignore: deprecated_member_use
-              title: Text(AppLocalizations.of(context).profile),
-            ),
-          ],
+          ),
         ),
       ),
     );
