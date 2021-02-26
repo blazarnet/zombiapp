8614ae32f764d873a8133bfe6bab90839a150480
Alan Alexis Moreno Martinez <alanalexismartinez@gmail.com>
2021-02-26 10:43:33 -0600

    Adición de los controller para la obtención de la data ingresa en los inputs para pasarla como parametro al evento que realizará el login por medio de un get

--- lib/src/pages/sign_in_page.dart
+++ lib/src/pages/sign_in_page.dart
@@ -1,17 +1,40 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:zombifi_app/generated/l10n.dart';
+import 'package:zombifi_app/src/bloc/provider.dart';
+import 'package:zombifi_app/src/controllers/login_controller.dart';
+import 'package:zombifi_app/src/model/login.dart';
 import 'package:zombifi_app/src/pages/main_page.dart';
 import 'package:zombifi_app/src/pages/sign_up_page.dart';
+import 'package:zombifi_app/src/providers/user_provider.dart';
 import 'package:zombifi_app/utils/utils.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 
 //Importaciones propias
 import 'package:zombifi_app/widgets/button_round.dart';
 import 'package:zombifi_app/widgets/text_form_field.dart';
 
-class SignInPage extends StatelessWidget {
+class SignInPage extends StatefulWidget {
+  @override
+  _SignInPageState createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  TextEditingController _userEmailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  // final _loginController = new LoginController();
+  Login _userLogin = Login();
+  final _userProvider = new UserProvider();
+
   final double _size = 18;
+
   @override
   Widget build(BuildContext context) {
+    // setState(() {
+    //   getPreferences();
+    // });
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
@@ -43,8 +66,6 @@ class SignInPage extends StatelessWidget {
     );
   }
 
-  //Método que será compuesto por los métodos que serán los que van a componer
-  //el body de la vista
   _widgetsSignIn(BuildContext context) => Container(
         child: Form(
           child: Column(
@@ -60,71 +81,84 @@ class SignInPage extends StatelessWidget {
         ),
       );
 
-  //Método que instancia los métodos que generan los textforms
-  _textFormFields(BuildContext context) => Container(
-        child: Column(
-          children: <Widget>[
-            Padding(padding: EdgeInsets.symmetric(vertical: 20)),
-            _email(context),
-            Padding(padding: EdgeInsets.only(top: 18)),
-            _password(context),
-          ],
-        ),
-      );
-  //TextFormField para el email
-  TextFormFieldWidget _email(BuildContext context) {
-    return TextFormFieldWidget(
-      controller: null,
-      keyboardType: TextInputType.emailAddress,
-      prefixIcon: Icon(
-        Icons.email_outlined,
-        color: Colors.black,
-        size: _size,
-      ),
-      hintText: 'ejemplo@correo.com',
-      labelText: AppLocalizations.of(context).email,
-      style: TextStyle(
-        fontFamily: "gibson",
-      ),
-      obscureText: false,
-    );
-  }
-
-  //TextFormField para el password
-  TextFormFieldWidget _password(BuildContext context) {
-    return TextFormFieldWidget(
-      controller: null,
-      obscureText: true,
-      prefixIcon: Icon(
-        Icons.lock_outline_rounded,
-        color: Colors.black,
-        size: _size,
-      ),
-      suffix: _lostPassword(context),
-      labelText: AppLocalizations.of(context).password,
-      style: TextStyle(
-        fontFamily: "gibson",
+  Widget _textFormFields(BuildContext context) {
+    final bloc = Provider.of(context);
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Padding(padding: EdgeInsets.symmetric(vertical: 20)),
+          _email(context, bloc),
+          Padding(padding: EdgeInsets.only(top: 18)),
+          _password(context, bloc),
+        ],
       ),
     );
   }
 
-  //Método que instancia los métodos que generan los buttons
-  _buttons(BuildContext context) => Container(
-        child: Column(
-          children: <Widget>[
-            // Padding(padding: EdgeInsets.only(top: 10)),
-            // // _lostPassword(context),
-            Padding(padding: EdgeInsets.only(top: 10)),
-            _signIn(context),
-            Padding(padding: EdgeInsets.only(top: 25)),
-            _signUp(context),
-            Padding(padding: EdgeInsets.only(top: 50)),
-            _orSignIn(context),
-          ],
-        ),
-      );
+  Widget _email(BuildContext context, LoginBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.emailStream,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        return TextFormFieldWidget(
+          controller: _userEmailController,
+          keyboardType: TextInputType.emailAddress,
+          prefixIcon: Icon(
+            Icons.email_outlined,
+            color: Colors.black,
+            size: _size,
+          ),
+          hintText: 'ejemplo@correo.com',
+          labelText: AppLocalizations.of(context).email,
+          style: TextStyle(
+            fontFamily: "gibson",
+          ),
+          obscureText: false,
+        );
+      },
+    );
+  }
 
-  //Método que importa el widget personalizado ButtonRound
+  Widget _password(BuildContext context, LoginBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.passwordStream,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        return TextFormFieldWidget(
+          controller: _passwordController,
+          keyboardType: TextInputType.text,
+          obscureText: true,
+          prefixIcon: Icon(
+            Icons.lock_outline_rounded,
+            color: Colors.black,
+            size: _size,
+          ),
+          // suffix: _lostPassword(context),
+          labelText: AppLocalizations.of(context).password,
+          style: TextStyle(
+            fontFamily: "gibson",
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buttons(BuildContext context) {
+    final bloc = Provider.of(context);
+    return Container(
+      child: Column(
+        children: <Widget>[
+          // Padding(padding: EdgeInsets.only(top: 10)),
+          // // _lostPassword(context),
+          Padding(padding: EdgeInsets.only(top: 10)),
+          _signIn(bloc, context),
+          Padding(padding: EdgeInsets.only(top: 25)),
+          _signUp(context),
+          Padding(padding: EdgeInsets.only(top: 50)),
+          _orSignIn(context),
+        ],
+      ),
+    );
+  }
+
   Widget _lostPassword(BuildContext context) {
     return GestureDetector(
       onTap: () {},
@@ -148,8 +182,7 @@ class SignInPage extends StatelessWidget {
     );
   }
 
-  //Método que importa el widget personalizado ButtonRound
-  ButtonRoundWidget _signIn(BuildContext context) {
+  ButtonRoundWidget _signIn(LoginBloc bloc, BuildContext context) {
     return ButtonRoundWidget(
       child: Text(
         AppLocalizations.of(context).signInUpper,
@@ -167,15 +200,14 @@ class SignInPage extends StatelessWidget {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(30),
       ),
-      onPressed: () {
-        final route = MaterialPageRoute(
-          builder: (context) {
-            return MainPage();
-          },
-        );
-        Navigator.push(context, route);
+      onPressed: () async {
+        loginUser(_userEmailController.text, _passwordController.text, context);
       },
     );
+  }
+
+  loginUser(String email, String password, BuildContext context) {
+    _userProvider.loginUser(email, password, context);
   }
 
   Widget _signUp(BuildContext context) {
@@ -299,4 +331,20 @@ class SignInPage extends StatelessWidget {
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
     );
   }
+
+  // Future getPreferences() async {
+  //   SharedPreferences preferences = await SharedPreferences.getInstance();
+
+  //   setState(() {
+  //     // ignore: unnecessary_statements
+  //     _userLogin.user = preferences.get("_userEmail") ?? "null";
+  //     _userLogin.password = preferences.get("_password") ?? "null";
+  //   });
+  // }
+
+  // Future savePreferences() async {
+  //   SharedPreferences preferences = await SharedPreferences.getInstance();
+  //   preferences.setString("_userEmail", _userEmailController.text);
+  //   preferences.setString("_password", _passwordController.text);
+  // }
 }
