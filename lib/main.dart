7b40b6cef28473b0ce97f3ca531c3a10f0fa4f71
Alan Alexis Moreno Martinez <alanalexismartinez@gmail.com>
2021-02-26 10:54:12 -0600

    Clase Provider para hacer un wrap de InheritedWidget al MaterialApp de la clase main

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/services.dart';
 //Mis importaciones
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:zombifi_app/generated/l10n.dart';
+import 'package:zombifi_app/src/bloc/provider.dart';
 import 'package:zombifi_app/src/pages/splash_screen.dart';
 import 'package:zombifi_app/utils/colors.dart';
 // import 'package:flutter_svg/flutter_svg.dart';
@@ -22,17 +23,19 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Zombifi App',
-      debugShowCheckedModeBanner: false,
-      localizationsDelegates: [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-        AppLocalizations.delegate
-      ],
-      supportedLocales: AppLocalizations.delegate.supportedLocales,
-      home: SplashScreen(),
+    return Provider(
+      child: MaterialApp(
+        title: 'Zombifi App',
+        debugShowCheckedModeBanner: false,
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+          AppLocalizations.delegate
+        ],
+        supportedLocales: AppLocalizations.delegate.supportedLocales,
+        home: SplashScreen(),
+      ),
     );
   }
 }
